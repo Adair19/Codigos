36b21dcd42430af1c08a88a37b3d777902b2a14e
Adair Corona Domingo <al221910501@gmail.com>
2020-09-18 08:38:42 -0500

    interpolacion.dart

---
+++ interpolacion.dart
@@ -0,0 +1,9 @@
+void main() {
+  String saludo = 'Hola';
+  String nombre = 'Adair Corona';
+  int edad = 19;
+  print('$saludo: $nombre, ¿Tienes $edad años?');
+  print('$nombre: tu numbre tiene ${nombre.length} caracteres');
+  print(
+      nombre + ': tu nombre tiene ' + nombre.length.toString() + ' caracteres');
+}
---
+++ tablas-multiplicar.dart
@@ -0,0 +1,10 @@
+void main() {
+  int tabla_del;
+  int por;
+  for (tabla_del = 1; tabla_del <= 10; tabla_del++) {
+    print("\nTabla del $tabla_del:");
+    for (por = 1; por <= 10; por++) {
+      print('$tabla_del * $por = ${tabla_del * por}');
+    }
+  }
+}
