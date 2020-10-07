72800dd39aeb3fd0c7f6e1d8b1f87371386704e1
Adair Corona Domingo <al221910501@gmail.com>
2020-10-06 20:49:32 -0500

    Animales

--- animales.dart
+++ animales.dart
@@ -131,9 +131,29 @@ class Fly {
   }
 }
 
+class Eat {
+  //métodos
+  void comer() {
+    print('estoy comiendo');
+  }
+}
+
+class Reproduce {
+  //métodos
+  void reproducir() {
+    print('me estoy reproduciendo');
+  }
+}
+
+class Gesta {
+  //métodos
+  void gestando() {
+    print('estoy gestando');
+  }
+}
 // Fin de clases que usaremos como mixin
 
-class Dolphin extends Mammal with Swim {
+class Dolphin extends Mammal with Swim, Eat, Reproduce, Gesta {
   Dolphin(
       {String color_pelo = 'gris',
       int glandulas_mamarias = 0,
@@ -168,11 +188,116 @@ class Dolphin extends Mammal with Swim {
             temperatura_corporal: temperatura_corporal);
 }
 
+class Bat extends Mammal with Fly, Eat, Reproduce {
+  //atributos
+  int longitud_pico;
+
+  Bat(
+      {this.longitud_pico = 0,
+      String nombre = 'desconocido',
+      String sexo = 'desconocido',
+      String color = 'desconocido',
+      String alimentacion = 'desconocido',
+      String habitat = 'desconocido',
+      String region = 'desconocido',
+      int altura = 0,
+      int peso = 0,
+      int edad = 0,
+      int longevidad = 0,
+      int numero_crias = 0,
+      int periodo_gestacion = 0,
+      int temperatura_corporal = 0})
+      : super(
+            nombre: nombre,
+            sexo: sexo,
+            color: color,
+            alimentacion: alimentacion,
+            habitat: habitat,
+            region: region,
+            altura: altura,
+            peso: peso,
+            edad: edad,
+            longevidad: longevidad,
+            numero_crias: numero_crias,
+            periodo_gestacion: periodo_gestacion,
+            temperatura_corporal: temperatura_corporal);
+}
+
+class Cat extends Mammal with Walk, Eat, Reproduce {
+  //atributos
+  int longitud_pico;
+
+  Cat(
+      {this.longitud_pico = 0,
+      String nombre = 'desconocido',
+      String sexo = 'desconocido',
+      String color = 'Blanco',
+      String alimentacion = 'desconocido',
+      String habitat = 'desconocido',
+      String region = 'desconocido',
+      int altura = 0,
+      int peso = 0,
+      int edad = 0,
+      int longevidad = 0,
+      int numero_crias = 0,
+      int periodo_gestacion = 0,
+      int temperatura_corporal = 0})
+      : super(
+            nombre: nombre,
+            sexo: sexo,
+            color: color,
+            alimentacion: alimentacion,
+            habitat: habitat,
+            region: region,
+            altura: altura,
+            peso: peso,
+            edad: edad,
+            longevidad: longevidad,
+            numero_crias: numero_crias,
+            periodo_gestacion: periodo_gestacion,
+            temperatura_corporal: temperatura_corporal);
+}
+
 class Bird extends Animal {
   //atributos
   int longitud_pico;
 
   Bird(
+      {this.longitud_pico = 0,
+      String nombre = 'desconocido',
+      String sexo = 'desconocido',
+      String color = 'desconocido',
+      String alimentacion = 'desconocido',
+      String habitat = 'desconocido',
+      String region = 'desconocido',
+      int altura = 0,
+      int peso = 0,
+      int edad = 0,
+      int longevidad = 0,
+      int numero_crias = 0,
+      int periodo_gestacion = 0,
+      int temperatura_corporal = 0})
+      : super(
+            nombre: nombre,
+            sexo: sexo,
+            color: color,
+            alimentacion: alimentacion,
+            habitat: habitat,
+            region: region,
+            altura: altura,
+            peso: peso,
+            edad: edad,
+            longevidad: longevidad,
+            numero_crias: numero_crias,
+            periodo_gestacion: periodo_gestacion,
+            temperatura_corporal: temperatura_corporal);
+}
+
+class Fish extends Animal {
+  //atributos
+  int longitud_pico;
+
+  Fish(
       {this.longitud_pico = 0,
       String nombre = 'desconocido',
       String sexo = 'desconocido',
@@ -236,21 +361,150 @@ class Duck extends Bird with Swim, Walk, Fly {
             temperatura_corporal: temperatura_corporal);
 }
 
+class Dove extends Bird with Walk, Fly, Eat, Reproduce {
+  Dove(
+      {int longitud_pico = 0,
+      String nombre = 'desconocido',
+      String sexo = 'desconocido',
+      String color = 'desconocido',
+      String alimentacion = 'desconocido',
+      String habitat = 'desconocido',
+      String region = 'desconocido',
+      int altura = 0,
+      int peso = 0,
+      int edad = 0,
+      int longevidad = 0,
+      int numero_crias = 0,
+      int periodo_gestacion = 0,
+      int temperatura_corporal = 0})
+      : super(
+            longitud_pico: longitud_pico,
+            nombre: nombre,
+            sexo: sexo,
+            color: color,
+            alimentacion: alimentacion,
+            habitat: habitat,
+            region: region,
+            altura: altura,
+            peso: peso,
+            edad: edad,
+            longevidad: longevidad,
+            numero_crias: numero_crias,
+            periodo_gestacion: periodo_gestacion,
+            temperatura_corporal: temperatura_corporal);
+}
+
+class Shark extends Fish with Swim, Eat, Reproduce {
+  Shark(
+      {int longitud_pico = 0,
+      String nombre = 'desconocido',
+      String sexo = 'desconocido',
+      String color = 'desconocido',
+      String alimentacion = 'desconocido',
+      String habitat = 'desconocido',
+      String region = 'desconocido',
+      int altura = 0,
+      int peso = 0,
+      int edad = 0,
+      int longevidad = 0,
+      int numero_crias = 0,
+      int periodo_gestacion = 0,
+      int temperatura_corporal = 0})
+      : super(
+            longitud_pico: longitud_pico,
+            nombre: nombre,
+            sexo: sexo,
+            color: color,
+            alimentacion: alimentacion,
+            habitat: habitat,
+            region: region,
+            altura: altura,
+            peso: peso,
+            edad: edad,
+            longevidad: longevidad,
+            numero_crias: numero_crias,
+            periodo_gestacion: periodo_gestacion,
+            temperatura_corporal: temperatura_corporal);
+}
+
+class FlyingFish extends Fish with Swim, Eat, Reproduce, Fly {
+  FlyingFish(
+      {int longitud_pico = 0,
+      String nombre = 'desconocido',
+      String sexo = 'desconocido',
+      String color = 'desconocido',
+      String alimentacion = 'desconocido',
+      String habitat = 'desconocido',
+      String region = 'desconocido',
+      int altura = 0,
+      int peso = 0,
+      int edad = 0,
+      int longevidad = 0,
+      int numero_crias = 0,
+      int periodo_gestacion = 0,
+      int temperatura_corporal = 0})
+      : super(
+            longitud_pico: longitud_pico,
+            nombre: nombre,
+            sexo: sexo,
+            color: color,
+            alimentacion: alimentacion,
+            habitat: habitat,
+            region: region,
+            altura: altura,
+            peso: peso,
+            edad: edad,
+            longevidad: longevidad,
+            numero_crias: numero_crias,
+            periodo_gestacion: periodo_gestacion,
+            temperatura_corporal: temperatura_corporal);
+}
+
 void main() {
-  // final Animal animal = Animal(nombre: 'Dumbo', sexo: 'Macho');
-  // print(animal);
+  //final Animal animal = Animal(nombre: 'Dumbo', sexo: 'Macho');
+  //print(animal);
 
-  // final Mammal elefante = Mammal(nombre: 'Dumbo', sexo: 'Hembra');
-  // final Dolphin delfin = Dolphin(nombre: 'Delfino', sexo: 'Hembra');
-  // delfin.comer();
-  // delfin.gestar();
-  // delfin.reproducir();
-  // delfin.amamantar();
-  // delfin.nadar();
+  //final Mammal elefante = Mammal(nombre: 'Dumbo', sexo: 'Hembra');
 
-  Duck lucas = Duck(nombre: 'Pato Lucas', sexo: 'Masculino');
+  Dolphin delfin = Dolphin(nombre: 'Delfin', sexo: 'Macho');
+  print(delfin);
+  delfin.comer();
+  delfin.nadar();
+
+  Cat gato = Cat(nombre: 'Gato', sexo: 'Hembra');
+  print(gato);
+  gato.caminar();
+  gato.comer();
+  gato.reproducir();
+
+  Bat vampiro = Bat(nombre: 'Murcielago', sexo: 'Hembra');
+  print(vampiro);
+  vampiro.volar();
+  vampiro.comer();
+  vampiro.reproducir();
+
+  Duck lucas = Duck(nombre: 'Pato Donald', sexo: 'Macho');
   print(lucas);
   lucas.nadar();
   lucas.caminar();
   lucas.volar();
+
+  Dove paloma = Dove(nombre: 'Paloma', sexo: 'Hembra');
+  print(paloma);
+  paloma.comer();
+  paloma.volar();
+  paloma.reproducir();
+
+  Shark tiburon = Shark(nombre: 'Tiburoncin', sexo: 'Macho');
+  print(tiburon);
+  tiburon.nadar();
+  tiburon.comer();
+  tiburon.reproducir();
+
+  FlyingFish pezvolador = FlyingFish(nombre: 'Pez Volador', sexo: 'Hembra');
+  print(pezvolador);
+  pezvolador.volar();
+  pezvolador.comer();
+  pezvolador.reproducir();
+  pezvolador.nadar();
 }
